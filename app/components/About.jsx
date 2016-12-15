c3cc4f737c0ca7d4b9aa6f530b89099ba0712db9
Chenxing <chenxing1991@yahoo.com>
2016-12-14 19:57:01 -0500

    uso profiles  update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,8 +9,9 @@ var About = (props) =>{
       <a className="expanded button" href="http://openweathermap.org/">Openweathermap</a>
       <p> Feel free to check it out my Git hub link also:</p>
       <a className="expanded button success" href="https://github.com/fistcat/ReactWeather/">GitHub Repo</a>
-      <p>My Bro's twitch.tv channel</p>
+      <p>My Bro's twitch.tv channel osu profiles</p>
       <a className="expanded button alert" href="https://www.twitch.tv/jjkkstar">James Twitch Channel</a>
+      <a className="expanded button alert hollow" href="https://osu.ppy.sh/u/7499518">James osu profiles</a>      
     </div>
   )
 };
